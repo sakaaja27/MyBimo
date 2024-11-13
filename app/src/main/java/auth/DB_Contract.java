7a38cc75301ebbc77fd49399be2f4aa8c1c3498d
Bramasta <bramastasaka9@gmail.com>
2024-11-13 09:59:56 +0700

    pl

--- app/src/main/java/auth/DB_Contract.java
+++ app/src/main/java/auth/DB_Contract.java
@@ -1,7 +1,10 @@
 package auth;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class DB_Contract {
     public static String ip = "10.10.180.244";
+    
     public static final String urlRegister = "http://"+ip+"/website%20mybimo/mybimo/src/koneksi/register.php";
     public static final String urlLogin = "http://"+ip+"/website%20mybimo/mybimo/src/koneksi/login.php";
 }
